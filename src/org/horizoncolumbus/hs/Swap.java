25bd90eae2aa054f0f291d6ee2c8cdb64e8ca4ad
HSA-Student <student.e.amofa@horiozncolumbus.org>
2021-03-24 00:41:04 -0400

    Initial commit

---
+++ src/org/horizoncolumbus/hs/Swap.java
@@ -0,0 +1,20 @@
+package org.horizoncolumbus.hs;
+
+public class Swap {
+
+    public static void main(String[] args){
+
+
+        int x = 9;
+        int y = 5;
+
+        int temp = x;
+        x = y;
+        y = temp;
+
+
+        System.out.println("x:" + x);
+        System.out.println("y:" + y);
+
+    }
+}
